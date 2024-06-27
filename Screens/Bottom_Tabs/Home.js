ed3aa376e691f8b9004a9067521a4d559dde140a
Alfie-1516 <okorochaalfred@gmail.com>
2024-06-27 13:44:50 -0500

    Updated home page

--- Screens/Bottom_Tabs/Home.js
+++ Screens/Bottom_Tabs/Home.js
@@ -1,13 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View className="flex flex-row justify-center bg-indigo-500 h-full">
-      <View>
-        <Text className="text-3xl text-[#50d71e] bg-black w-screen">Home</Text>
+    <SafeAreaView style={styles.container}>
+      <View className="flex flex-col justify-start overflow-y-scroll  h-full ">
+        <View className="bg-gray-600 basis-1/12">
+        </View>
+        <View className="bg-red-900 basis-1/6">
+
+        </View>
+        <View className="bg-orange-500 basis-2/3">
+
+        </View>
+        <View className="bg-lime-500 basis-16">
+
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
