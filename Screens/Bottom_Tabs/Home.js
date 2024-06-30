6b82bec5f0fe1d080306ecd5d88f1b903ac4fb79
Alfie-1516 <okorochaalfred@gmail.com>
2024-06-30 09:34:53 -0500

    updated_home

--- Screens/Bottom_Tabs/Home.js
+++ Screens/Bottom_Tabs/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <SafeAreaView className="flex flex-col  justify-start  h-screen  items-center bg-black">
       {/* This is the top nav bar  */}
-      <View className=" basis-1/12 flex  w-screen  items-center border-solid border-b bg-white border-[#989898] pb-5">
+      <View className=" basis-14 flex  w-screen  items-center border-solid border-b bg-white border-[#989898] ">
         <View className=" flex flex-row w-11/12 justify-between">
           <View className="basis-2/6 items-start justify-center ">
             <AntDesign name="book" size={24} color="black" />
@@ -32,19 +32,19 @@ const Home = () => {
         <View className="basis-1/4 w-screen flex justify-center items-center ">
           <View className=" flex flex-col w-11/12 justify-end items-start mt-3 mb-4">
             <Text className=" text-3xl text-left">Hi Dushan</Text>
-            <Text className="mt-3">June 04, 2024</Text>
+            <Text className="mt-3 text-sm">June 04, 2024</Text>
           </View>
         </View>
         {/* This class contains the dashboard, todays task, and on-campus live events */}
         <View className=" basis-2/3 w-screen h-screen items-center  ">
           <View className="flex  w-11/12 h-full justify-start">
-            <View className="flex basis-1/12 flex-row items-center justify-between mb-3">
-              <Text className="text-l font-medium">DashBoard</Text>
+            <View className="flex basis-1/12 flex-row items-center justify-between ">
+              <Text className="text-base font-medium">DashBoard</Text>
               <AntDesign name="ellipsis1" size={24} color="black" />
             </View>
             {/* Todays task and Live events on campus */}
             {/* Today's Tasks */}
-            <View className="flex  basis-2/6 rounded-xl  justify-between items-center border-solid border border-[#989898] overflow-hidden mb-5">
+            <View className="flex  basis-1/4 rounded-xl  justify-between items-center border-solid border border-[#989898] overflow-hidden mb-5">
               {/* The code below is for the Todays task header */}
               <View className="basis-1/6 w-full justify-center items-center bg-[#075eec] ">
                 <View className="w-11/12 justify-center ">
@@ -52,19 +52,7 @@ const Home = () => {
                 </View>
               </View>
               {/* Tasks view below shows the 3 task in todays tasks */}
-              <View className=" flex basis-2/5 w-11/12 justify-center">
-                <View className="basis-fit flex flex-row ">
-                  <View className="justify-start mr-3">
-                    <AntDesign name="checksquareo" size={20} color="black" />
-                  </View>
-                  <View className="basis-4/5 w-4/5 justify-start">
-                    <Text>Submit Lab Assignment</Text>
-                    <View className="flex flex-row items-center">
-                      <Ionicons name="time-outline" size={15} color="#075eec" />
-                      <Text className="ml-2 text-[#075eec]">10:30 PM</Text>
-                    </View>
-                  </View>
-                </View>
+              <View className=" flex basis-1/4 w-11/12 justify-center">
                 <View className="basis-fit flex flex-row  mt-3">
                   <View className="justify-start mr-3">
                     <AntDesign name="checksquareo" size={20} color="black" />
@@ -99,7 +87,7 @@ const Home = () => {
               </View>
             </View>
             {/* Live Events on Campus */}
-            <View className="flex  basis-2/6 rounded-xl  justify-between items-center border-solid border  border-[#989898] overflow-hidden mb-5">
+            <View className="flex  basis-1/4 rounded-xl  justify-between items-center border-solid border border-[#989898] overflow-hidden mb-5">
               {/* The code below is for the Todays task header */}
               <View className="basis-1/6 w-full justify-center items-center bg-[#075eec] ">
                 <View className="w-11/12 justify-center ">
@@ -107,19 +95,7 @@ const Home = () => {
                 </View>
               </View>
               {/* Tasks view below shows the 3 task in todays tasks */}
-              <View className=" flex basis-2/5 w-11/12 justify-center">
-                <View className="basis-fit flex flex-row ">
-                  <View className="justify-start mr-3">
-                    <AntDesign name="checksquareo" size={20} color="black" />
-                  </View>
-                  <View className="basis-4/5 w-4/5 justify-start">
-                    <Text>Submit Lab Assignment</Text>
-                    <View className="flex flex-row items-center">
-                      <Ionicons name="time-outline" size={15} color="#075eec" />
-                      <Text className="ml-2 text-[#075eec]">10:30 PM</Text>
-                    </View>
-                  </View>
-                </View>
+              <View className=" flex basis-1/4 w-11/12 justify-center">
                 <View className="basis-fit flex flex-row  mt-3">
                   <View className="justify-start mr-3">
                     <AntDesign name="checksquareo" size={20} color="black" />
@@ -146,7 +122,7 @@ const Home = () => {
                 </View>
               </View>
               {/* This code below is for the View more button */}
-              <View className="basis-1/6 w-full justify-center items-center border-solid border-t border-[#989898] rounded-t-l ">
+              <View className="basis-1/6 w-full justify-center items-center border-solid border-[#989898] border-t rounded-t-l ">
                 <View className="w-11/12 justify-between flex flex-row ">
                   <Text className="text-black">View More</Text>
                   <AntDesign name="right" size={15} color="black" />
