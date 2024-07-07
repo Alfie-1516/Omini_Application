a9e066f4fd8f14f858b504a6e2ffa6db1d44f97d
Alfie-1516 <okorochaalfred@gmail.com>
2024-07-07 15:33:33 -0500

    Fiixed Acitvites page

--- Screens/Bottom_Tabs/Activities.js
+++ Screens/Bottom_Tabs/Activities.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, Image, View, ScrollView } from "react-native";
-import React from 'react'
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
@@ -7,9 +7,9 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Activites = () => {
   return (
-    <SafeAreaView className="flex flex-col  justify-start  h-screen  items-center bg-black">
+    <SafeAreaView className="flex h-screen bg-white">
       {/* This is the top nav bar  */}
-      <View className=" basis-1/12 flex  w-screen  items-center border-solid border-b bg-white border-[#989898] pb-5">
+      <View className=" basis-1/12 flex  w-screen  items-center border-solid border-b bg-white border-[#989898]">
         <View className=" flex flex-row w-11/12 justify-between">
           <View className="basis-2/6 items-start justify-center ">
             <AntDesign name="book" size={24} color="black" />
@@ -27,97 +27,124 @@ const Activites = () => {
           </View>
         </View>
       </View>
-      <ScrollView className="flex basis-4/5 bg-white ">
+      <ScrollView className="flex  h-screen ">
         {/* Events */}
-        <View className="basis-1/4 w-screen flex justify-center items-center">
+        <View className=" h-fit flex justify-center items-center ">
           <View className="flex flex-row w-11/12 justify-between items-center mt-3 mb-4">
-              <Text className="text-3xl" >Events</Text>
-              <AntDesign name="ellipsis1" size={24} color="black" />
+            <Text className="text-3xl">Events</Text>
+            <AntDesign name="ellipsis1" size={24} color="black" />
           </View>
         </View>
-
         {/* class for today*/}
-        <View className=" basis-2/3 items-center  ">
-          <View className="flex  w-11/12 h-full justify-start">
-            <View className="flex basis-1/12 flex-row items-center justify-between mb-3">
+        <View className=" h-52 items-center  ">
+          <View className="flex  w-11/12 h-full justify-start ">
+            <View className="flex h-1/12 flex-row items-center justify-between mb-3 ">
               <Text className="text-1 font-medium">Today</Text>
             </View>
             {/* Today's events */}
-            <View className="flex  basis-2/6 rounded-xl  justify-between items-center border-solid border border-[#989898] overflow-hidden mb-5">
+            <View className="flex  h-fit rounded-xl  justify-between items-center border-solid border border-[#989898]  ">
               {/* elements*/}
-  
-              <View className="flex basis-2/5 w-11/12 justify-center">
-              <Text className="text-blue-700 text-[15px] font-medium font-['Inter']">SAT JUN 9 - 8:45 AM</Text>
-              <Text className="text-black text-[15px] font-medium font-['Inter']">Pick up soccer game</Text>
-              <Text className="text-neutral-700 text-[15px] font-normal font-['Inter']"><FontAwesome name="map-pin" size={20} color="#656565"/>  McNeese Recreational Center</Text>
-              <Text className="text-neutral-700 text-[15px] font-normal font-['Inter']">10 Attendees</Text>
+
+              <View className="flex basis-2/5 w-11/12 justify-center  mt-2">
+                <Text className="text-blue-700 text-[15px] font-medium ">
+                  SAT JUN 9 - 8:45 AM
+                </Text>
+                <Text className="text-black text-[15px] font-medium ">
+                  Pick up soccer game
+                </Text>
+                <Text className="text-neutral-700 text-[15px] font-normal ">
+                  <FontAwesome name="map-pin" size={20} color="#656565" />{" "}
+                  McNeese Recreational Center
+                </Text>
+                <Text className="text-neutral-700 text-[15px] font-normal ">
+                  10 Attendees
+                </Text>
               </View>
               {/* This code below is for the View more button */}
-              <View className="basis-1/6 w-full justify-center items-center border-solid border-[#989898] border-t rounded-t-l ">
-                <View className="w-11/12 justify-between flex flex-row ">
+              <View className="flex h-1/4 w-full items-center justify-center  border-solid border-[#989898] border-t ">
+                <View className="w-11/12  flex flex-row justify-between items-center ">
                   <Text className="text-black">View More</Text>
                   <AntDesign name="right" size={15} color="black" />
                 </View>
               </View>
             </View>
-            
           </View>
         </View>
-        {/*second one*/}
-        <View className=" basis-2/3 items-center  ">
-          <View className="flex  w-11/12 h-full justify-start">
-            <View className="flex basis-1/12 flex-row items-center justify-between mb-3">
-              <Text className="text-1 font-medium">Tommorow</Text>
+        <View className=" h-52 items-center  ">
+          <View className="flex  w-11/12 h-full justify-start ">
+            <View className="flex h-1/12 flex-row items-center justify-between mb-3 ">
+              <Text className="text-1 font-medium">Today</Text>
             </View>
-            <View className="flex  basis-2/6 rounded-xl  justify-between items-center border-solid border border-[#989898] overflow-hidden mb-5">
-              {/* events*/}
-              <View className="flex basis-2/5 w-11/12 justify-center">
-              <Text className="text-blue-700 text-[15px] font-medium font-['Inter']">SUN JUN 10 - 5:00 PM</Text>
-              <Text className="text-black text-[15px] font-medium font-['Inter']">Basketball open practice</Text>
-              <Text className="text-neutral-700 text-[15px] font-normal font-['Inter']">  McNeese Legacy Center</Text>
-              <Text className="text-neutral-700 text-[15px] font-normal font-['Inter']">53 Attendees</Text><FontAwesome name="" size={20} color="#656565"/>
+            {/* Today's events */}
+            <View className="flex  h-fit rounded-xl  justify-between items-center border-solid border border-[#989898]  ">
+              {/* elements*/}
+
+              <View className="flex basis-2/5 w-11/12 justify-center  mt-2">
+                <Text className="text-blue-700 text-[15px] font-medium ">
+                  SAT JUN 9 - 8:45 AM
+                </Text>
+                <Text className="text-black text-[15px] font-medium ">
+                  Pick up soccer game
+                </Text>
+                <Text className="text-neutral-700 text-[15px] font-normal ">
+                  <FontAwesome name="map-pin" size={20} color="#656565" />{" "}
+                  McNeese Recreational Center
+                </Text>
+                <Text className="text-neutral-700 text-[15px] font-normal ">
+                  10 Attendees
+                </Text>
               </View>
               {/* This code below is for the View more button */}
-              <View className="basis-1/6 w-full justify-center items-center border-solid border-[#989898] border-t rounded-t-l ">
-                <View className="w-11/12 justify-between flex flex-row ">
+              <View className="flex h-1/4 w-full items-center justify-center  border-solid border-[#989898] border-t ">
+                <View className="w-11/12  flex flex-row justify-between items-center ">
                   <Text className="text-black">View More</Text>
                   <AntDesign name="right" size={15} color="black" />
                 </View>
               </View>
             </View>
-            
           </View>
         </View>
-        {/*Third one*/}
-        <View className=" basis-2/3 items-center  ">
-          <View className="flex  w-11/12 h-full justify-start">
-            <View className="flex basis-1/12 flex-row items-center justify-between mb-3">
-              <Text className="text-1 font-medium">Upcoming</Text>
+        <View className=" h-52 items-center  ">
+          <View className="flex  w-11/12 h-full justify-start ">
+            <View className="flex h-1/12 flex-row items-center justify-between mb-3 ">
+              <Text className="text-1 font-medium">Today</Text>
             </View>
-            <View className="flex  basis-2/6 rounded-xl  justify-between items-center border-solid border border-[#989898] overflow-hidden mb-5">  
-              {/* events*/}
-              <View className="flex basis-2/5 w-11/12 justify-center">
-              <Text className="text-blue-700 text-[15px] font-medium font-['Inter']">MON JUN 11 - 12:00 PM</Text>
-              <Text className="text-black text-[15px] font-medium font-['Inter']">NSBE Meeting</Text>
-              <Text className="text-neutral-700 text-[15px] font-normal font-['Inter']"><FontAwesome name="map-pin" size={20} color="#656565"/>  Mcneese Drew Room 125</Text>
-              <Text className="text-neutral-700 text-[15px] font-normal font-['Inter']">17 Attendees</Text>
+            {/* Today's events */}
+            <View className="flex  h-fit rounded-xl  justify-between items-center border-solid border border-[#989898]  ">
+              {/* elements*/}
+
+              <View className="flex basis-2/5 w-11/12 justify-center  mt-2">
+                <Text className="text-blue-700 text-[15px] font-medium ">
+                  SAT JUN 9 - 8:45 AM
+                </Text>
+                <Text className="text-black text-[15px] font-medium ">
+                  Pick up soccer game
+                </Text>
+                <Text className="text-neutral-700 text-[15px] font-normal ">
+                  <FontAwesome name="map-pin" size={20} color="#656565" />{" "}
+                  McNeese Recreational Center
+                </Text>
+                <Text className="text-neutral-700 text-[15px] font-normal ">
+                  10 Attendees
+                </Text>
               </View>
               {/* This code below is for the View more button */}
-              <View className="basis-1/6 w-full justify-center items-center border-solid border-[#989898] border-t rounded-t-l ">
-                <View className="w-11/12 justify-between flex flex-row ">
+              <View className="flex h-1/4 w-full items-center justify-center  border-solid border-[#989898] border-t ">
+                <View className="w-11/12  flex flex-row justify-between items-center ">
                   <Text className="text-black">View More</Text>
                   <AntDesign name="right" size={15} color="black" />
                 </View>
               </View>
             </View>
-            
           </View>
         </View>
+        
+        
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
-export default Activites
+export default Activites;
 
-const styles = StyleSheet.create({}) 
+const styles = StyleSheet.create({});
